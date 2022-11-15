a17a39f2d0c0d3eff9d3535577b65640fc53dd0a
Aaroh Srivastava <aarohsjcian1@gmail.com>
2022-11-15 19:02:43 +0530

    multer setup and updateuser route done

---
+++ src/config/multer.filefilter.config.ts
@@ -0,0 +1,18 @@
+import { Request } from 'express'
+import  multer,{ FileFilterCallback } from 'multer'
+import path from "path"
+export default multer({
+  storage: multer.diskStorage({}),
+  fileFilter: (
+    request: Request,
+    file: Express.Multer.File,
+    callback: FileFilterCallback
+): void => {
+  let ext = path.extname(file.originalname);
+  if (ext !== ".jpg" && ext !== ".jpeg" && ext !== ".png") {
+    callback(null, false);
+    return;
+  }
+  callback(null, true);
+}
+}); 
