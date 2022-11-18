e95068843dfb41e0a35c64102da943f7d60ac81a
metal-oopa <sudip5banerjee1974@gmail.com>
2022-11-19 02:25:14 +0530

    update

--- src/routes/cron.ts
+++ src/routes/cron.ts
@@ -7,7 +7,9 @@ router.post("/api/cron", async (req: Request, res: Response) => {
   try {
     console.log("Running cron job...");
 
-    sendNFTsToTopScorers();
+    await sendNFTsToTopScorers();
+
+    res.status(201).send("Cron job started");
   } catch (e) {
     res.status(404).send(e);
     console.log(e);
