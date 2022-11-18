85a117785f13a3166764c0920b8f173f675e2a8c
metal-oopa <sudip5banerjee1974@gmail.com>
2022-11-19 02:07:30 +0530

    add cron job route

---
+++ src/routes/cron.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from "express";
+import { sendNFTsToTopScorers } from "../services/sendNFT";
+
+const router = express.Router();
+
+router.post("/api/cron", async (req: Request, res: Response) => {
+  try {
+    console.log("Running cron job...");
+
+    sendNFTsToTopScorers();
+  } catch (e) {
+    res.status(404).send(e);
+    console.log(e);
+  }
+});
+export { router as checkwalletRouter };
