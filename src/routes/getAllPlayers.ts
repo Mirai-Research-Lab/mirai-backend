aa6069ac239e6207481c3924833dd98119890380
metal-oopa <sudip5banerjee1974@gmail.com>
2022-11-18 22:18:40 +0530

    catch errors in /api/players route

--- src/routes/getAllPlayers.ts
+++ src/routes/getAllPlayers.ts
@@ -4,16 +4,18 @@ import { auth } from "../middleware/tokenauth";
 
 const router = express.Router();
 
-router.get(
-  "/api/players",
-  /* auth,  */ async (req: Request, res: Response) => {
+router.get("/api/players", async (req: Request, res: Response) => {
+  try {
     const players = await Player.find({}).sort({
       highest_score: -1,
       updatedAt: 1,
     });
 
     res.send(players);
+  } catch (err) {
+    console.log(err);
+    res.status(400).send({ message: err.message });
   }
-);
+});
 
 export { router as getAllPlayersRouter };
