b8ded29653e86871893172f5cf5ca56452775d98
metal-oopa <sudip5banerjee1974@gmail.com>
2022-11-18 22:03:26 +0530

    add try catch in token authentication

--- src/middleware/tokenauth.ts
+++ src/middleware/tokenauth.ts
@@ -2,25 +2,32 @@ import express, { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 
 async function auth(req: Request, res: Response, next: NextFunction) {
-  console.log(req.headers.cookies);
-  if (
-    req.headers &&
-    req.headers.cookies &&
-    req.headers.cookies !== "jwt=undefined"
-  ) {
-    const token = req.headers.cookies.toString().split("=")[1];
-    const decoded = jwt.verify(token.toString(), process.env.JWT_KEY!);
-    if (!decoded) {
-      //If some error occurs
-      res.status(400).json({
-        error: "User not Signed in, Sign in First.",
-      });
+  try {
+    console.log(req.headers.cookies);
+    if (
+      req.headers &&
+      req.headers.cookies &&
+      req.headers.cookies !== "jwt=undefined"
+    ) {
+      const token = req.headers.cookies.toString().split("=")[1];
+      const decoded = jwt.verify(token.toString(), process.env.JWT_KEY!);
+      if (!decoded) {
+        //If some error occurs
+        res.status(400).json({
+          error: "User not Signed in, Sign in First.",
+        });
+      } else {
+        req.email = decoded["email"].value;
+      }
+      next();
     } else {
-      req.email = decoded["email"].value;
+      res.send({ currentuser: null });
     }
-    next();
-  } else {
-    res.send({ currentuser: null });
+  } catch (e) {
+    console.log(e);
+    res.status(400).json({
+      error: "Malformed jwt token",
+    });
   }
 }
 export { auth as auth };
